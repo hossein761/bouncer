f19fc2cc226e45bdd9d0c938b34c389cf95ff026
Hossein Kazemi <mrhosseinkazemi@gmail.com>
2014-05-31 17:48:19 +0200

    Added attribution comment to passwordhash

--- app/utils/PasswordHash.java
+++ app/utils/PasswordHash.java
@@ -38,7 +38,7 @@ import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-/*
+/** This is an adaptation based on  the below work:
  * PBKDF2 salted password hashing.
  * Author: havoc AT defuse.ca
  * www: http://crackstation.net/hashing-security.htm
