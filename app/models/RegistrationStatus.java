9b02b11371667e57594a120152b89007fa6ef930
Hossein Kazemi <mrhosseinkazemi@gmail.com>
2014-06-01 12:42:13 +0200

    Added registrationstatus enum

---
+++ app/models/RegistrationStatus.java
@@ -0,0 +1,10 @@
+package models;
+
+/**
+ * // TODO: Add class description here.
+ *
+ * @author Hossein Kazemi <a href="mailto:mrhosseinkazemi@gmail.com">mrhosseinkazemi@gmail.com</a>
+ */
+public enum RegistrationStatus {
+    PENDING,REGISTERED
+}
