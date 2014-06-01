72d88628d92a3a51748f57d9efe7a82d0f9f6005
Hossein Kazemi <mrhosseinkazemi@gmail.com>
2014-06-01 12:43:07 +0200

    Added ebeans annotations + RegistrationStatus column to RegistrationToken model + finder method + findbyId

--- app/models/RegistrationToken.java
+++ app/models/RegistrationToken.java
@@ -2,7 +2,9 @@ package models;
 
 import play.db.ebean.Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -13,10 +15,24 @@ import javax.persistence.Table;
 @Entity()
 @Table(name = "registration")
 public class RegistrationToken extends Model {
+    @Id
+    @Column(name = "id")
     public String id;
+    @Column(name = "name")
     public String name;
+    @Column(name = "lastName")
     public String lastName;
+    @Column(name = "email")
     public String email;
+    @Column(name = "status")
+    public RegistrationStatus status;
+
+    private static Finder<String, RegistrationToken> find = new Finder<String, RegistrationToken>(String.class,
+                                                                                                  RegistrationToken.class);
     public RegistrationToken() {
+    }
+
+    public static RegistrationToken findById(final String id){
+        return find.byId(id);
     }
 }
