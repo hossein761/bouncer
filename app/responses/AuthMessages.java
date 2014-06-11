93c5d13c9b4722e92094d947d7cbedb1227f1cfe
Hossein Kazemi <mrhosseinkazemi@gmail.com>
2014-06-11 21:59:16 +0200

    Added AuthMessages

---
+++ app/responses/AuthMessages.java
@@ -0,0 +1,30 @@
+package responses;
+
+/**
+ * Messages from the Auth service (not the errors!)
+ * @author Hossein Kazemi <a href="mailto:mrhosseinkazemi@gmail.com">mrhosseinkazemi@gmail.com</a>
+ */
+public enum AuthMessages {
+    CHECK_EMAIL("1000","Check you mailbox"),
+    REGISTRATION_COMPLETE("1001","Registration is now complete. You may login"),
+    LOGGED_OUT("1001","You have been logged out");
+
+
+
+    private final String messageCode;
+    private final String messageText;
+
+    AuthMessages(String messageCode, String messageText) {
+        this.messageCode = messageCode;
+        this.messageText = messageText;
+    }
+
+    public String getMessageCode() {
+        return messageCode;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+}
