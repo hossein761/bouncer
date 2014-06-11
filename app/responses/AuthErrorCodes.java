03d253d3eeae1f705ebd80836311aeb8f5fcc8d8
Hossein Kazemi <mrhosseinkazemi@gmail.com>
2014-06-11 21:44:42 +0200

    Added more AuthError codes

--- app/responses/AuthErrorCodes.java
+++ app/responses/AuthErrorCodes.java
@@ -7,19 +7,20 @@ package responses;
  */
 public enum AuthErrorCodes {
 
-    //access denied
     ACCESS_DENIED("100", "you are not logged in"),
-    //auth code not valid
     AUTH_CODE_NOT_VALID("101", "Auth-code is invalid"),
-    // registration incomplete
     REGISTRATION_INCOMPLETE("102", "Registration is not complete yet!"),
-    //invalid credentials
     INVALID_CREDENTIALS("103", "Invalid credentials"),
-    //username already taken
     USER_NAME_ALREADY_TAKEN("104", "Username already taken"),
-    //email already taken
     EMAIL_ALREADY_TAKEN("105", "Email already taken"),
-    INVALID_REGISTRATION_TOKEN("106", "You should signup first");
+    INVALID_REGISTRATION_TOKEN("106", "You should signup first"),
+    FIRST_NAME_EMPTY("200", "First name cannot be empty"),
+    LAST_NAME_EMPTY("201", "Last name cannot be empty"),
+    USER_NAME_EMPTY("202","User name cannot be empty"),
+    PASSWORD_EMPTY("203","Password cannot be empty"),
+    EMAIL_EMPTY("204","Email cannot be empty"),
+    EMAIL_NOT_WELL_FORMED("205","Malformed email address"),
+    EMAIL_OR_USER_NAME_EMPTY("206","Email/Username cannot be empty");
 
     private final String errorCode;
     private final String errorMessage;
